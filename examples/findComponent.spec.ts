6d134f53eaecc8c4244fab58a854e6bde78e6ada
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-01-27 13:18:11 +1000

    wip: findComponent

---
+++ examples/findComponent.spec.ts
@@ -0,0 +1,47 @@
+import { defineComponent, h, render } from 'vue'
+
+import { mount } from '../framework'
+
+const CompA = defineComponent({
+  name: 'CompA',
+  render() {
+    return h('div', { id: 'comp-a' }, [this.$slots?.default()])
+  }
+})
+
+const CompB = defineComponent({
+  name: 'CompB',
+  render() {
+    return h('div', { id: 'comp-b' }, [this.$slots?.default()])
+  }
+})
+
+const CompC = defineComponent({
+  name: 'CompC',
+  render() {
+    return h('div', { id: 'comp-c' }, [this.$slots?.default()])
+  }
+})
+
+
+test('findComponent', () => {
+  const Component = defineComponent({
+    setup() {
+      return () =>
+        h('div', {}, [
+          h(CompA, {}, () => 
+            h(CompB, {}, () => 
+              h(CompC, {}, () => 'Component C')
+            )
+          )
+        ])
+    }
+  })
+
+  const wrapper = mount(Component)
+
+  expect(wrapper.find('#comp-c')).toBeTruthy()
+  expect(wrapper.find('#comp-c').text()).toBe('Component C')
+
+  wrapper.findComponent('CompC')
+})
